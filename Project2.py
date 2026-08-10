cdd05f65a500aa94ef4ec825ed9d32eb2d86b591
Tejaspcal <tejasp.cal@gmail.com>
2026-08-09 20:02:31 -0700

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,15 @@
+#print birthdays of friends
+Dob1 ="May 10"
+Dob2 = "December 20"
+Dob3 = "July 7"
+Dob4 = "March 10"
+Dob5 = "October 10"
+print(Dob1)
+print(Dob2)
+print(Dob3)
+print(Dob4)
+print(Dob5)
+print(Dob1, Dob2, Dob3, Dob4, Dob5)
+print("My birthday is on", Dob1, "and my friends birthday is on", Dob2, "and my other friends birthday is on", Dob3, "and my other friends birthday is on", Dob4, "and my other friends birthday is on", Dob5)
+x = "December 31"
+print(x)
