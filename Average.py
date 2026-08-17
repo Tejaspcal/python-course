0326e1afab620b7f6e4965c559a3753446632fa7
Tejaspcal <tejasp.cal@gmail.com>
2026-08-16 19:34:13 -0700

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,10 @@
+pizza1 = 45
+pizza2 = 30
+pizza3 =57
+pizza4 = 60
+pizza5 = 87
+
+total = pizza1 + pizza2 + pizza3 + pizza4 + pizza5
+average = total / 5
+print("the average pizza give is:", average)
+print("the total pizzas give is:", total)
