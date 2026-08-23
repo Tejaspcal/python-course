6c63efb76875fbdcbd0c7f6b604fe32016969541
Tejaspcal <tejasp.cal@gmail.com>
2026-08-23 10:21:58 -0700

    Add files via upload

---
+++ Project_8232026.py
@@ -0,0 +1,6 @@
+Weather = int(input("Enter the weather: "))
+
+if (Weather < 70):
+    print(" The weather is less than 70 degrees so wear a jacket and long pants")
+else:
+    print(" The weather is greater than or equal to 70 degrees so wear a t-shirt and shorts")
