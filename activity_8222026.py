0bfa2b357e8d741c0d1bd675e169a44cba65582b
Tejaspcal <tejasp.cal@gmail.com>
2026-08-22 17:33:33 -0700

    Add files via upload

---
+++ activity_8222026.py
@@ -0,0 +1,8 @@
+A = int(input("Enter number: "))
+if (A < 49):
+    print("A is smaller than 49")
+    print("i'm in if block")
+else:
+    print("A is greater than 49")
+    print("i'm in else block")
+    print("i'm not in if and not in else block")
