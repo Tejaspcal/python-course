00c2ddaaa2511d4ead1d64402a8a413741e49816
Tejaspcal <tejasp.cal@gmail.com>
2026-08-23 19:39:46 -0700

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,25 @@
+T = 44
+Y = 55
+Z = 0
+
+if T and Y and Z:
+    print("All conditions are true")
+else:
+    print("At least one condition is false")
+
+
+
+
+A = 10
+B = 20
+W = 0
+
+if  A > 0 or B > 0:
+    print("All conditions are true")
+else:
+    print("At least one condition is false")
+
+if  B > 0 or W > 0:
+    print("All conditions are true")
+else:
+    print("At least one condition is false")
