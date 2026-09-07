696aafbfc400f702e0e171737414fd3f7ec0b09f
Tejaspcal <tejasp.cal@gmail.com>
2026-09-07 10:35:15 -0700

    Add files via upload

---
+++ Checkage.py
@@ -0,0 +1,7 @@
+E = int(input("Enter the your age: "))
+
+if (E >= 10 and E <= 20):
+
+    print("You are eligible to enrol this class")
+else:
+    print("You are not eligible to enrol this class")
