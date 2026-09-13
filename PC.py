6148edbecb7c2420c363b30d4ef5b6b61dfd5c2c
Tejaspcal <tejasp.cal@gmail.com>
2026-09-13 12:14:02 -0700

    Add files via upload

---
+++ PC.py
@@ -0,0 +1,5 @@
+numbersign = int(input("Enter the number sign for which you want to find the power: "))
+powersign = int(input("Enter the power sign oveleasly: "))
+
+Answer = numbersign ** powersign
+print(f"{numbersign} to the power of {powersign} is {Answer}")
