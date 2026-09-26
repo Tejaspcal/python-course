f4652ba6c8293bf0739eb06e07baa83a924aed21
Tejaspcal <tejasp.cal@gmail.com>
2026-09-25 19:27:05 -0700

    Add files via upload

---
+++ Star.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("brown")
+border = turtle.Turtle()
+
+border.forward(100)
+
+border.left(120)
+border.forward(100)
+
+border.left(120)
+border.forward(100)
+
+border.penup()
+border.right(150)
+border.forward(50)
+
+border.pendown()
+border.right(90)
+border.forward(100)
+
+border.right(120)
+border.forward(100)
+
+border.right(120)
+border.forward(100)
+
+turtle.done()
